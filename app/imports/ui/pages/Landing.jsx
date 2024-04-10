fbe7d9a695a0a61fe6139bbffd6d479c7b302c68
nsching <143790749+nsching@users.noreply.github.com>
2024-04-09 19:17:54 -1000

    Added text for Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
     <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Aloha!</h1>
-        <p>Write description here</p>
+      <Col className="d-flex flex-column justify-content-center">
+        <h1>ALOHA MAI KĀKOU!</h1>
+        <p> </p>
+        <h2>`Imi means “search” in ʻŌlelo Hawaiʻi</h2>
+        <p> </p>
+        <p className="px-5">Invasive species pose a significant threat to local ecosystems, causing ecological imbalances or even extinction.
+          However, monitoring and tracking these invasive species across the Hawaiian islands can be a daunting and resource-expensive
+          task, making research difficult to complete.
+        </p>
+        <p className="px-5">‘Imi Invasive aims to address invasive species management challenges by
+          establishing a citizen science initiative to bring the community together and educate people on invasive species, raising
+          awareness and empowering the community and professionals to be able to better track the harmful species and care for the
+          ecosystem. It should serve as an educational resource and also a centralized app for people to report and help manage invasive
+          species across the Hawaiian Islands.
+        </p>
       </Col>
 
     </Row>
