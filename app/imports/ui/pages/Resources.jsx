d65b3d885bd4f94c6e756b049892a53a69617f8c
Gian <gckp@hawaii.edu>
2024-04-09 18:34:10 -1000

    testing merge to main

---
+++ app/imports/ui/pages/Resources.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import NavBar from '../components/NavBar';
+
+const Resources = () => (
+  <Container>
+    <NavBar />
+  </Container>
+);
+
+export default Resources;
