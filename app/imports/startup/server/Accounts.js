5a274a1980129af0f46db7849cdd9f206c8c43e1
Myra Ortigosa <ortigosa@hawaii.edu>
2024-04-10 21:15:05 -1000

    updated page
    Took 1 hour 37 minutes

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
+import { Profiles } from '../../api/profile/Profile.js';
 
 /* eslint-disable no-console */
 
@@ -16,13 +17,21 @@ const createUser = (email, password, role) => {
     Roles.addUsersToRoles(userID, 'admin');
   }
 };
+// Initialize ProfilesCollection default data
+const createProfile = (firstName, lastName, email, bio, interests) => {
+  console.log(`  Creating profile for ${email}.`);
+  Profiles.collection.insert({ firstName, lastName, email, bio, interests });
+};
 
 // When running app for first time, pass a settings file to set up a default user account.
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.forEach(({ email, password, role }) => createUser(email, password, role));
-  } else {
+    Meteor.settings.defaultAccounts.forEach(({ email, password, role, firstName, lastName, bio, interests }) => {
+      createUser(email, password, role);
+      createProfile(firstName, lastName, email, bio, interests);
+    });
+} else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
 }
--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Stuffs } from '../../api/stuff/Stuff';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { Profiles } from '../../api/profile/Profile';
 
-const Profile = () => (
-  <Container className="py-3">
-    <Row className="justify-content-center">
-      <Col xs={4} className="text-center">
-        <h2>
-          <p>Profile</p>
-        </h2>
-      </Col>
-    </Row>
-  </Container>
-);
+//edit and view your own profile
+const Profile = () => {
+  const currentUser = useTracker(() => Meteor.user());
+  const { ready, thisProfile } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(Profiles.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Stuff documents
+    const profile = Profiles.collection.findOne({ email: currentUser?.username });
+    return {
+      thisProfile: profile,
+      ready: rdy,
+    };
+  }, []);
+  return ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={4} className="text-center">
+          <h2>Profile</h2>
+          <Card>
+            <Card.Body>
+              <Card.Text>{thisProfile.firstName}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />;
+};
 
 export default Profile;
