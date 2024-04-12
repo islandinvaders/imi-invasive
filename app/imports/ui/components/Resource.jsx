5b69a0aadf07f23d6865a298f957524b7655ce2a
Gian <gckp@hawaii.edu>
2024-04-11 17:00:38 -1000

    Added more default entries and image component to resources

--- app/imports/ui/components/Resource.jsx
+++ app/imports/ui/components/Resource.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardBody, CardFooter } from 'react-bootstrap';
+import { Card, CardBody, CardFooter, Col, Image, Row } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const Resource = ({ resource }) => (
   <Card className="h-100">
     <Card.Header>
-      <Card.Title>{resource.pestName}</Card.Title>
-      <Card.Subtitle>
-        {resource.sciName}
-      </Card.Subtitle>
+      <Col>
+        <Card.Title>{resource.pestName}</Card.Title>
+        <Card.Subtitle>
+          {resource.sciName}
+        </Card.Subtitle>
+      </Col>
     </Card.Header>
     <CardBody>
-      <Card.Text><strong>Risk:</strong> {resource.risk}</Card.Text>
-      <Card.Text><strong>Reg Status:</strong> {resource.regStatus}</Card.Text>
-      <Card.Text><strong>Pest Type:</strong> {resource.pestType}</Card.Text>
-      <Card.Text><strong>Description:</strong> {resource.description}</Card.Text>
-      <Card.Text><strong>Impact:</strong> {resource.impact}</Card.Text>
-      <Card.Text><strong>Distribution:</strong> {resource.distribution}</Card.Text>
+      <Row>
+        <Col>
+          <Card.Text><strong>Risk:</strong> {resource.risk}</Card.Text>
+          <Card.Text><strong>Reg Status:</strong> {resource.regStatus}</Card.Text>
+          <Card.Text><strong>Pest Type:</strong> {resource.pestType}</Card.Text>
+          <Card.Text><strong>Description:</strong> {resource.description}</Card.Text>
+          <Card.Text><strong>Impact:</strong> {resource.impact}</Card.Text>
+          <Card.Text><strong>Distribution:</strong> {resource.distribution}</Card.Text>
+        </Col>
+        <Col className="text-end"><Image src={resource.image} width={300} /></Col>
+      </Row>
     </CardBody>
     <CardFooter>
       <Card.Text><strong>Lookalike:</strong> {resource.lookalike}</Card.Text>
@@ -28,6 +35,7 @@ const Resource = ({ resource }) => (
 // Require a document to be passed to this component.
 Resource.propTypes = {
   resource: PropTypes.shape({
+    image: PropTypes.string,
     pestName: PropTypes.string,
     sciName: PropTypes.string,
     risk: PropTypes.string,
