857da6d18112ebd7baf5d0336bcc5f31ccafba02
Gian <gckp@hawaii.edu>
2024-04-13 10:11:31 -1000

    Updated search

--- app/imports/ui/pages/Resources.jsx
+++ app/imports/ui/pages/Resources.jsx
@@ -1,53 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row, Form } from 'react-bootstrap';
 import { Search } from 'react-bootstrap-icons';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import LoadingSpinner from '../components/LoadingSpinner';
 import Resource from '../components/Resource';
 import { References } from '../../api/reference/Reference';
 
 const Resources = () => {
-  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, references } = useTracker(() => {
-    // Note that this subscription will get cleaned up
-    // when your component is unmounted or deps change.
-    // Get access to Stuff documents.
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const references = useTracker(() => {
     const subscription = Meteor.subscribe(References.userPublicationName);
-    // Determine if the subscription is ready
-    const rdy = subscription.ready();
-    // Get the References documents
-    const referenceItems = References.collection.find({}).fetch();
-    return {
-      references: referenceItems,
-      ready: rdy,
-    };
-  }, []);
-  return (ready ? (
+
+    if (!subscription.ready()) {
+      return [];
+    }
+
+    let referenceItems = [];
+    if (searchQuery.trim() === '') {
+      referenceItems = References.collection.find({}).fetch();
+    } else {
+      referenceItems = References.collection.find({
+        pestName: { $regex: searchQuery.trim(), $options: 'i' },
+      }).fetch();
+    }
+
+    return referenceItems;
+  }, [searchQuery]);
+
+  return (
     <Container className="py-3">
       <Row className="justify-content-center">
-        <Col>
+        <Col className="text-center">
+          <h2>Resources</h2>
+        </Col>
+        <Row>
           <Col>
-            <h2 className="text-center">Resources</h2>
-          </Col>
-          <Col>
-            <Form className="d-flex justfity-content-end m-1">
-              <Search size={20} className="mt-2 mr-2 m-1" />
+            <Form onSubmit={(e) => e.preventDefault()} className="d-flex align-items-center justify-content-end">
+              <Search className="m-1" size="20" />
               <Form.Control
                 type="text"
-                placeholder="Search by name"
-                className="input-sm justfity-content-end"
-                style={{ width: '150px' }} // Adjust the width here
+                placeholder="Search by pest name"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="input-sm"
+                style={{ width: '200px', marginRight: '10px' }}
               />
             </Form>
           </Col>
-          <Row xl={1} classname="g-4">
-            {references.map((reference) => (<Col className="m-3" key={reference._id}><Resource resource={reference} /></Col>))}
-          </Row>
-        </Col>
+        </Row>
+        <Row xl={1} className="g-2">
+          {references.map((reference) => (
+            <Col className="m-3" key={reference._id}>
+              <Resource resource={reference} />
+            </Col>
+          ))}
+        </Row>
       </Row>
     </Container>
-  ) : <LoadingSpinner />);
+  );
 };
 
 export default Resources;
