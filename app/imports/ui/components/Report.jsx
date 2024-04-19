fe7f62ee75b5d3a4225fbe686d942b704244282a
yangyunshi <113159604+yangyunshi@users.noreply.github.com>
2024-04-18 18:07:20 -1000

    Posts now load from collection

---
+++ app/imports/ui/components/Report.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image, ListGroup } from 'react-bootstrap';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const Report = ({ report }) => (
+  <Card>
+    <Card.Header>
+      <Image src={report.image} />
+      <Card.Title>{report.pestName}</Card.Title>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{report.pestDescription}</Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroup.Item>{report.island} </ListGroup.Item>
+      <ListGroup.Item>{report.location}</ListGroup.Item>
+      <ListGroup.Item>{report.date.toLocaleDateString()}</ListGroup.Item>
+      <ListGroup.Item>{report.reporter}</ListGroup.Item>
+      <ListGroup.Item>{report.verified}</ListGroup.Item>
+      <ListGroup.Item>{report.removed}</ListGroup.Item>
+    </ListGroup>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+Report.propTypes = {
+  report: PropTypes.shape({
+    image: PropTypes.string,
+    pestName: PropTypes.string,
+    pestDescription: PropTypes.string,
+    island: PropTypes.string,
+    location: PropTypes.string,
+    date: PropTypes.instanceOf(Date),
+    reporter: PropTypes.string,
+    verified: PropTypes.string,
+    removed: PropTypes.string,
+    // _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default Report;
--- app/imports/ui/pages/Posts.jsx
+++ app/imports/ui/pages/Posts.jsx
@@ -1,54 +1,37 @@
 import React from 'react';
-import { Col, Container, Image, Row, Button, ButtonGroup, DropdownButton, Dropdown, Card, ListGroup } from 'react-bootstrap';
-import DownloadButton from '../components/DownloadButton.jsx';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Reports } from '../../api/report/Report';
+import LoadingSpinner from '../components/LoadingSpinner';
+import Report from '../components/Report';
 
-/* A simple static component to render some text for the landing page. */
-const Posts = () => (
-  <Container fluid className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Row className="d-flex justify-content-center align-items-center">
-          <Image roundedCircle src="https://m.media-amazon.com/images/I/812Onuail2L._AC_UF894,1000_QL80_.jpg" />
-        </Row>
-        <Row className="d-flex justify-content-center align-items-center">
-          <ButtonGroup vertical style={{ width: '150px' }}> {/* Set a fixed width for the button group */}
-            <DropdownButton
-              as={ButtonGroup}
-              title="My Posts"
-              id="bg-vertical-dropdown-1"
-            >
-              <Dropdown.Item eventKey="1">View All</Dropdown.Item>
-              <Dropdown.Item eventKey="2">Delete</Dropdown.Item>
-              <Dropdown.Item eventKey="3">Edit</Dropdown.Item>
-            </DropdownButton>
-            <Button>Everyone Else</Button>
-            <DownloadButton />
-          </ButtonGroup>
-        </Row>
-      </Col>
+const Posts = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { ready, reports } = useTracker(() => {
+    // Note that this subscription will get cleaned up
+    // when your component is unmounted or deps change.
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(Reports.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Report documents
+    const reportItems = Reports.collection.find({}).fetch();
+    return {
+      reports: reportItems,
+      ready: rdy,
+    };
+  }, []);
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>POSTS</h1>
-        <Col className="d-flex justify-content-center align-items-center">
-          <Card style={{ width: '50rem' }}>
-            <Card.Img variant="top" src="https://allthatsinteresting.com/wordpress/wp-content/uploads/2022/06/angry-mongoose.jpg" />
-            <Card.Body>
-              <Card.Title>WILDCARD</Card.Title>
-              <Card.Text>
-                Short description of what the poster saw/experienced
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>Species: </ListGroup.Item>
-              <ListGroup.Item>Spotted at: </ListGroup.Item>
-              <ListGroup.Item>Other: </ListGroup.Item>
-            </ListGroup>
-          </Card>
+  return (ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col className="text-center">
+          <h2>Posts </h2>
         </Col>
-      </Col>
-
-    </Row>
-  </Container>
-);
-
+      </Row>
+      {reports.map((report) => (<Row className="py-4" key={report._id}><Report report={report} /></Row>))}
+    </Container>
+  ) : <LoadingSpinner />);
+};
 export default Posts;
