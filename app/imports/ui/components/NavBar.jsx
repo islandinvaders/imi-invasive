36e6cbc7864ed7bb290bb89fdcbced73955b1154
nsching <143790749+nsching@users.noreply.github.com>
2024-04-23 19:49:58 -1000

    update navbar.jsx

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -12,24 +12,26 @@ const NavBar = () => {
   }), []);
 
   return (
-    <Navbar expand="lg" className="custom-navbar justify-content-center">
-      <Container className="justify-content-center">
+    <Navbar expand="lg" className="custom-navbar">
+      <Container>
+        <Navbar.Brand id="landing-page" as={NavLink} to="/">
+          <h2>&apos;Imi Invasive</h2>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="justify-content-center">
             {currentUser && !Roles.userIsInRole(Meteor.userId(), 'admin') ? ([ // User Navbar
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/profile" key="profile">PROFILE</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/about" key="about">ABOUT US</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/posts" key="posts">POSTS</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/add" key="posts">FILE REPORT</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/resources" key="resources">INVASIVE LIST</Nav.Link>,
-            ]) : ''}
+              <Nav.Link id="profile-nav" as={NavLink} to="/profile" key="profile">Profile</Nav.Link>,
+              <Nav.Link id="about-us-nav" as={NavLink} to="/about" key="about">About Us</Nav.Link>,
+              <Nav.Link id="posts-nav" as={NavLink} to="/posts" key="posts">Posts</Nav.Link>,
+              <Nav.Link id="reports-nav" as={NavLink} to="/add" key="posts">File Report</Nav.Link>,
+              <Nav.Link id="resources-nav" as={NavLink} to="/resources" key="resources">Invasive List</Nav.Link>,]) : ''}
             {Roles.userIsInRole(Meteor.userId(), 'admin') ? ([ // Admin Navbar
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/profile" key="profile">Profile</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/about" key="about">ABOUT US</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/postsadmin" key="posts">POSTS ADMIN</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/add" key="posts">FILE REPORT</Nav.Link>,
-              <Nav.Link id="list-stuff-nav" as={NavLink} to="/resourcesadmin" key="resources">INVASIVE LIST ADMIN</Nav.Link>,
+              <Nav.Link id="profile-nav" as={NavLink} to="/profile" key="profile">Profile</Nav.Link>,
+              <Nav.Link id="about-us-nav" as={NavLink} to="/about" key="about">About Us</Nav.Link>,
+              <Nav.Link id="posts-admin-nav" as={NavLink} to="/postsadmin" key="posts">Posts Admin</Nav.Link>,
+              <Nav.Link id="posts-nav" as={NavLink} to="/add" key="posts">File Report</Nav.Link>,
+              <Nav.Link id="resources-admin-nav" as={NavLink} to="/resourcesadmin" key="resources">Invasive List Admin</Nav.Link>,
             ]) : ''}
           </Nav>
         </Navbar.Collapse>
