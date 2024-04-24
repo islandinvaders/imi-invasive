7695eb18879290d87c41ff64dd0d7091308fe1a4
lilyenan <143849309+lilyenan@users.noreply.github.com>
2024-04-23 20:55:22 -1000

    Update Login.jsx

--- app/imports/ui/components/Login.jsx
+++ app/imports/ui/components/Login.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
+import { Nav, NavDropdown } from 'react-bootstrap';
+import { BoxArrowRight, PersonFill } from 'react-bootstrap-icons';
 import '/client/style.css';
 
 const Login = () => {
@@ -12,30 +12,27 @@ const Login = () => {
   }), []);
 
   return (
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="custom-login-dropdown">
+    <Nav className="custom-login-dropdown">
       {currentUser === '' ? (
         <NavDropdown id="login-dropdown" className="custom-login-dropdown" title="Login">
           <NavDropdown.Item id="login-dropdown-sign-in" as={NavLink} to="/signin">
-            <PersonFill/>
+            <PersonFill />
             Sign in
           </NavDropdown.Item>
           <NavDropdown.Item id="login-dropdown-sign-up" as={NavLink} to="/signup">
-            <PersonPlusFill/>
+            <PersonFill />
             Sign up
           </NavDropdown.Item>
         </NavDropdown>
       ) : (
         <NavDropdown className="custom-login-dropdown" id="navbar-current-user" title={currentUser}>
           <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
-            <BoxArrowRight/>
+            <BoxArrowRight />
             Sign out
           </NavDropdown.Item>
         </NavDropdown>
       )}
     </Nav>
-    </Navbar.Collapse>
   );
 };
 
