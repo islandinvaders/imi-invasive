0b2bc075140909441a03b9144b281e6417bebf52
nsching <143790749+nsching@users.noreply.github.com>
2024-04-28 22:36:58 -1000

    finished landing page aesthetics

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -22,15 +22,15 @@ const Landing = () => (
                 className="img-fluid d-inline-block align-top landing-collage"
               />
             </Col>
-            <Col>
-              <h2 className="landing-header text-left">
+            <Col xs={12} md={6} className="text-left">
+              <h2 className="landing-header">
                 Help us ʻimi invasive species!
               </h2>
-              <p className="landing-description text-left">Invasive species pose a significant threat to local ecosystems, causing ecological imbalances or even extinction.
+              <p className="landing-description">Invasive species pose a significant threat to local ecosystems, causing ecological imbalances or even extinction.
                 However, monitoring and tracking these invasive species across the Hawaiian islands can be a daunting and resource-expensive
                 task, making research difficult to complete.
               </p>
-              <p className="landing-description text-left">‘Imi Invasive aims to address invasive species management challenges by
+              <p className="landing-description">‘Imi Invasive aims to address invasive species management challenges by
                 establishing a citizen science initiative to bring the community together and educate people on invasive species, raising
                 awareness and empowering the community and professionals to be able to better track the harmful species and care for the
                 ecosystem. It should serve as an educational resource and also a centralized app for people to report and help manage invasive
