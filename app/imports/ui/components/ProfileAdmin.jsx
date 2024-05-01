87bb8c75cc0d89ef8503eb4a64eabac22cea9ce3
Myra Ortigosa <ortigosa@hawaii.edu>
2024-04-30 15:52:50 -1000

    tried
    Took 35 minutes

--- app/imports/ui/components/ProfileAdmin.jsx
+++ app/imports/ui/components/ProfileAdmin.jsx
@@ -2,48 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image, Button } from 'react-bootstrap';
 
-/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const ProfileAdmin = ({ listProfile }) => (
-  <Card style={{ marginBottom: '20px', minHeight: '20rem', maxHeight: '20rem' }}>
-    <Card.Header>
-      {listProfile.image && (
-        <Image
-          roundedCircle
-          src={listProfile.image}
-          style={{ width: '80px', height: '80px' }}
-          alt="profile picture"
-        />
-      )}
-      <Card.Title>{listProfile.firstName} {listProfile.lastName}</Card.Title>
-      <Card.Subtitle>{listProfile.email}</Card.Subtitle>
-    </Card.Header>
-    <Card.Body style={{ maxHeight: '15rem', overflow: 'hidden' }}>
-      <span
-        className="overflow-y-hidden"
-        style={{
-          display: '-webkit-box',
-          maxHeight: '15rem',
-          WebkitBoxOrient: 'vertical',
-          WebkitLineClamp: 3, // Limits to 3 lines of text
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-        }}
-      >
-        <span className="fw-bold">Bio:</span>{' '}{listProfile.bio}
-      </span>
-      <Card.Text>
-        <span className="fw-bold">Interests:</span><span>{' '}{listProfile.interests}</span>
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <div className="d-flex justify-content-center">
-        <Button href={`/view-profile/${listProfile._id}`}>View Profile</Button>
-      </div>
-    </Card.Footer>
-  </Card>
-);
+const ProfileAdmin = ({ listProfile, onDelete }) => {
+  const removeItem = () => {
+    onDelete.remove(listProfile);
+  };
 
-// Require a document to be passed to this component.
+  return (
+    <Card style={{ marginBottom: '20px', minHeight: '20rem', maxHeight: '20rem' }}>
+      <Card.Header>
+        {listProfile.image && (
+          <Image
+            roundedCircle
+            src={listProfile.image}
+            style={{ width: '80px', height: '80px' }}
+            alt="profile picture"
+          />
+        )}
+        <Card.Title>{listProfile.firstName} {listProfile.lastName}</Card.Title>
+        <Card.Subtitle>{listProfile.email}</Card.Subtitle>
+      </Card.Header>
+      <Card.Body style={{ maxHeight: '15rem', overflow: 'hidden' }}>
+        <span
+          className="overflow-y-hidden"
+          style={{
+            display: '-webkit-box',
+            maxHeight: '15rem',
+            WebkitBoxOrient: 'vertical',
+            WebkitLineClamp: 3, // Limits to 3 lines of text
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+        >
+          <span className="fw-bold">Bio:</span>{' '}{listProfile.bio}
+        </span>
+        <Card.Text>
+          <span className="fw-bold">Interests:</span><span>{' '}{listProfile.interests}</span>
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <div className="d-flex justify-content-between">
+          <Button id="profile-admin-delete" variant="danger" onClick={() => removeItem(listProfile._id)}>Delete</Button>
+        </div>
+      </Card.Footer>
+    </Card>
+  );
+};
+
 ProfileAdmin.propTypes = {
   listProfile: PropTypes.shape({
     image: PropTypes.string,
@@ -55,6 +59,7 @@ ProfileAdmin.propTypes = {
     interests: PropTypes.string,
     _id: PropTypes.string.isRequired,
   }).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default ProfileAdmin;
